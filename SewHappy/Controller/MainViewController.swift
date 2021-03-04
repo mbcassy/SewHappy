6dd4c8bd3d5a3e9bd850be4b2e80615298e3cf58
cassandrabalbuena <mbcassy2298@gmail.com>
2021-03-04 18:53:58 -0500

    Project setup.

---
+++ SewHappy/Controller/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  SewHappy
+//
+//  Created by Cassy on 3/4/21.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SewHappy/SceneDelegate.swift
+++ SewHappy/SceneDelegate.swift
@@ -13,10 +13,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Captures scene, if there is one, into a constant
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create a new UIWindow using the captured scene above
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Create view hierarchy
+        let tabBarController = MainViewController()
+        
+        // Set the root view controller of the window with the newly created root view controller
+        window.rootViewController = tabBarController
+        
+        // Set the window and make it KeyAndVisible
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
