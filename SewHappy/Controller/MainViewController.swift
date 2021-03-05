41e4424f66c7393ae8ce1335fcf8a378fca4c4e6
cassandrabalbuena <mbcassy2298@gmail.com>
2021-03-04 20:31:20 -0500

    Added tab bar items.

--- SewHappy/Controller/MainViewController.swift
+++ SewHappy/Controller/MainViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class MainViewController: UITabBarController {
+class MainViewController: UITabBarController, UITabBarControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        self.delegate = self
+        view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
     
@@ -26,4 +27,21 @@ class MainViewController: UITabBarController {
     }
     */
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let fabricTab = FabricStashViewController()
+        let fabricTabBarItem = UITabBarItem(title: "Stash", image: UIImage(systemName: "scissors"), selectedImage: UIImage(systemName: "scissors"))
+        fabricTab.tabBarItem = fabricTabBarItem
+        
+        let projectsTab = ProjectsViewController()
+        let projectsTabBarItem = UITabBarItem(title: "Projects", image: UIImage(systemName: "folder.fill"), selectedImage: UIImage(systemName: "folder.fill"))
+        projectsTab.tabBarItem = projectsTabBarItem
+        
+        let settingsTab = SettingsViewController()
+        let settingsTabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gearshape.2.fill"), selectedImage: UIImage(systemName: "gearshape.2.fill"))
+        settingsTab.tabBarItem = settingsTabBarItem
+        
+        self.viewControllers = [fabricTab, projectsTab, settingsTab]
+    }
 }
---
+++ SewHappy/Controller/ProjectsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProjectsViewController.swift
+//  SewHappy
+//
+//  Created by Cassy on 3/4/21.
+//
+
+import UIKit
+
+class ProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
