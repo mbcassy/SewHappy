ff27db0bee351083b9fa29422aa15382a39024bd
cassandrabalbuena <mbcassy2298@gmail.com>
2021-03-05 19:55:18 -0500

    Added text label and button to the fabric stash view controller.

--- SewHappy/Controller/FabricStashViewController.swift
+++ SewHappy/Controller/FabricStashViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class FabricStashViewController: UIViewController {
 
     weak var fabricCollectionView: UICollectionView!
+    weak var cvTitle: UILabel!
+    weak var addButton: UIButton!
+    
     var data: [Int] = Array(0..<10)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.fabricCollectionView.backgroundColor = .white
         self.fabricCollectionView.dataSource = self
         self.fabricCollectionView.delegate = self
@@ -25,18 +27,52 @@ class FabricStashViewController: UIViewController {
     override func loadView() {
         super.loadView()
         
+        let padding:CGFloat = 85
+        let textLabel = UILabel(frame: .zero)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            textLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            textLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+        textLabel.text = "Fabric Stash"
+        self.cvTitle = textLabel
+        
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(collectionView)
         NSLayoutConstraint.activate([
-            self.view.topAnchor.constraint(equalTo: collectionView.topAnchor),
-            self.view.bottomAnchor.constraint(equalTo: collectionView.bottomAnchor),
-            self.view.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor),
-            self.view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: self.cvTitle.bottomAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
         ])
         self.fabricCollectionView = collectionView
+        
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(button)
+        //TODO: Edit these constraints so that the button resizes to the correct proportions for each device
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 75),
+            button.heightAnchor.constraint(equalToConstant: 75),
+            button.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        ])
+
+        button.clipsToBounds = true
+        button.backgroundColor = .black
+        //edit this so that the button image does not go to the edge of button. Basically make the button image smaller than button
+        button.setBackgroundImage(UIImage(systemName: "plus"), for: .normal)
+        
+        self.addButton = button
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.addButton.layer.cornerRadius = 0.5 * self.addButton.bounds.size.width
     }
 }
 
@@ -67,17 +103,25 @@ extension FabricStashViewController: UICollectionViewDelegate {
 
 extension FabricStashViewController: UICollectionViewDelegateFlowLayout {
 
-    //TODO: Fix this method to make perfectly square cells 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let numberOfCols = CGFloat(2)
-        let width = (collectionView.frame.size.width - (numberOfCols * view.frame.size.width / 15))/numberOfCols
-        let height = collectionView.frame.size.height / 5
-        return CGSize(width:width, height:height);
+        
+        let numberOfItemsPerRow:CGFloat = 2
+        let spacingBetweenCells:CGFloat = 16
+        let spacing:CGFloat = 16
+
+        let totalSpacing = (2 * spacing) + ((numberOfItemsPerRow - 1) * spacingBetweenCells) //Amount of total spacing in a row
+        
+        if let collection = self.fabricCollectionView {
+            let width = (collection.bounds.width - totalSpacing) / numberOfItemsPerRow
+            return CGSize(width: width, height: width)
+        } else {
+            return CGSize(width: 0, height: 0)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let cellWidthPadding = collectionView.frame.size.width / 20
-        let cellHeightPadding = collectionView.frame.size.height / 10
-        return UIEdgeInsets(top: cellHeightPadding,left: cellWidthPadding, bottom: cellHeightPadding,right: cellWidthPadding)
+        
+        let spacing:CGFloat = 16
+        return UIEdgeInsets(top: spacing,left: spacing, bottom: spacing,right: spacing)
     }
 }
--- SewHappy/View/FabricCell.swift
+++ SewHappy/View/FabricCell.swift
@@ -11,7 +11,8 @@ class FabricCell: UICollectionViewCell {
     static var identifier: String = "FabricCell"
 
     weak var textLabel: UILabel!
-
+    weak var bgImage: UIImageView!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -20,9 +21,21 @@ class FabricCell: UICollectionViewCell {
         self.contentView.addSubview(textLabel)
         NSLayoutConstraint.activate([
             self.contentView.centerXAnchor.constraint(equalTo: textLabel.centerXAnchor),
-            self.contentView.centerYAnchor.constraint(equalTo: textLabel.centerYAnchor),
+            self.contentView.centerYAnchor.constraint(equalTo: textLabel.centerYAnchor)
         ])
         self.textLabel = textLabel
+        
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            self.contentView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            self.contentView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor),
+            self.contentView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor),
+            self.contentView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor)
+        ])
+        self.bgImage = imageView
+        
         self.reset()
     }
 
@@ -37,5 +50,8 @@ class FabricCell: UICollectionViewCell {
 
     func reset() {
         self.textLabel.textAlignment = .center
+        self.bgImage.contentMode = .scaleAspectFill
+        self.bgImage.clipsToBounds = true
+        self.bgImage.layer.cornerRadius = 12
     }
 }
