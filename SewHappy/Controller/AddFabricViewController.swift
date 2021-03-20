9b22b19547b2337853e19cc66dd6a0a7a275c8ee
cassandrabalbuena <mbcassy2298@gmail.com>
2021-03-20 14:15:45 -0400

    Moved YPImagePicker presentation to inside the addButtonPressed function.

--- SewHappy/Controller/AddFabricViewController.swift
+++ SewHappy/Controller/AddFabricViewController.swift
@@ -10,35 +10,18 @@ import YPImagePicker
 
 class AddFabricViewController: UIViewController {
 
+    //TODO: Add functionality for when the cancel button is tapped
+    //TODO: Add functionality for when the next button is tapped
+    //TODO: Add functionality for when the user selects pictures
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    override func loadView() {
-        super.loadView()
-        var config = YPImagePickerConfiguration()
-        //config.library.maxNumberOfItems = 5
-        config.wordings.libraryTitle = "Library"
-        config.wordings.cameraTitle = "Camera"
-        config.wordings.next = "Next"
-        let picker = YPImagePicker(configuration: config)
-        picker.didFinishPicking { [unowned picker] items, cancelled in
-            if let photo = items.singlePhoto {
-                print(photo.fromCamera) // Image source (camera or library)
-                print(photo.image) // Final image selected by the user
-                print(photo.originalImage) // original image selected by the user, unfiltered
-                print(photo.modifiedImage) // Transformed image, can be nil
-                print(photo.exifMeta) // Print exif meta data of original image.
-            }
-            
-            if cancelled {
-                print("Picker was canceled")
-            }
-            picker.dismiss(animated: true, completion: nil)
-        }
-        present(picker, animated: true, completion: nil)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
 
 }
--- SewHappy/Controller/FabricStashViewController.swift
+++ SewHappy/Controller/FabricStashViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class FabricStashViewController: UIViewController {
 
@@ -26,5 +27,31 @@ class FabricStashViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         self.mainView.addButton.layer.cornerRadius = 0.5 * self.mainView.addButton.bounds.size.width
+        self.mainView.addButton.addTarget(self, action: #selector(addButtonPressed), for: .touchDown)
+    }
+    
+    @objc private func addButtonPressed(sender: UIButton) {        
+        var config = YPImagePickerConfiguration()
+        //config.library.maxNumberOfItems = 5
+        config.wordings.libraryTitle = "Library"
+        config.wordings.cameraTitle = "Camera"
+        config.wordings.next = "Next"
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, cancelled in
+            if let photo = items.singlePhoto {
+                print(photo.fromCamera) // Image source (camera or library)
+                print(photo.image) // Final image selected by the user
+                print(photo.originalImage) // original image selected by the user, unfiltered
+                print(photo.modifiedImage) // Transformed image, can be nil
+                print(photo.exifMeta) // Print exif meta data of original image.
+            }
+            
+            if cancelled {
+                picker.dismiss(animated: true, completion: nil)
+            }
+            picker.dismiss(animated: true, completion: nil)
+            
+        }
+        present(picker, animated: true, completion: nil)
     }
 }
